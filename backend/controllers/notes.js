de8fffd1f45175d8b99f625a360fe8e9025a4b0a
Matthew-Kwan <matthew.k913@gmail.com>
2021-01-13 00:39:37 -0500

    Added GET and POST requests

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -7,4 +7,24 @@ notesRouter.get('/', async (request, response) => {
   response.json(notes)
 })
 
+// HTTP GET ID REQUEST
+notesRouter.get('/:id', async (request, response) => {
+
+  const note = notes.findById(request.params.id)
+  response.json(note)
+
+})
+
 // HTTP POST REQUEST
+notesRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  const note = new Note({
+    title: body.title,
+    content: body.content,
+    date: new Date()
+  })
+
+  const savedNote = await note.save()
+  response.status(201).json(savedNote)
+})
